778e09406971a3bb1ed78b107322c04bef03c421
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2022-11-25 22:45:22 +0200

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// Write your code here\
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2
+console.log (multiply)
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+const num3 = 4;
+const num4 = 8;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(20, 1, 2, 6, 4)
+console.log(max)
